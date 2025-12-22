e7fb5df80839a0920c42f851e3a84afb039569d6
DORAEMONlovesMICHAN <mushamsushanth@gmail.com>
2025-12-23 00:33:15 +0530

    Initial commit - realtime editor backend

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import DocumentPage from "./documentPage";
+
+export default function App() {
+  return <DocumentPage docId="test-doc-123" />;
+}
---
+++ src/documentPage.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { socket } from "./socket";
+
+export default function DocumentPage({ docId }) {
+  const [document, setDocument] = useState("");
+
+  useEffect(() => {
+    socket.connect();
+
+    socket.emit("join-document", docId);
+
+    socket.on("receive-changes", (content) => {
+      setDocument(content);
+    });
+
+    return () => {
+      socket.off("receive-changes");
+      socket.disconnect();
+    };
+  }, [docId]);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setDocument(value);
+
+    socket.emit("send-changes", {
+      docId,
+      content: value
+    });
+  };
+
+  return (
+    <textarea
+      value={document}
+      onChange={handleChange}
+      style={{ width: "400px", height: "300px" }}
+    />
+  );
+}
